82eab3d53abe312b316800cdc7a5a491dacc87d2
Steven Green <sg1sg2sg3@hotmail.co.uk>
2015-10-15 14:26:41 +0100

    Correctly set up the meta data setting script as well as the script for downloading the output files.

--- MarlinJobs/DiracDownloads/Download.sh
+++ MarlinJobs/DiracDownloads/Download.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-source ../../env.sh
+source ../env.sh
 
-recoVar=68
-detModel=38
-evtType=Photon
-jobDescription=HighEnergyPhotons
+evtType=Z_uds
+jobDescription=OptimisationStudies
 
-for i in 1000 1500 # 10 20 50 100 200 500
-do 
-    cd /r06/lc/sg568/${jobDescription}/Detector_Model_${detModel}/Reco_Stage_${recoVar}/${i}GeV 
-    dirac-ilc-find-in-FC /ilc/user/s/sgreen JobDescription=${jobDescription} Energy=${i} EvtType=${evtType} Type=Rec MokkaJobNumber=${detModel} ReconstructionVariant=${recoVar} > "tmp.txt"
-    while read line 
+for detModel in {38..39} #{38..77}
+do
+    for recoStage in {69..76}
     do
-        if [[ $line == *'.root'* ]]
-        then
-            if [ ! -f "${line##*/}" ] && [ ! -f "/r06/lc/sg568/${jobDescription}/Detector_Model_${detModel}/Reco_Stage_${recoVar}/${i}GeV/${line##*/}" ];
-            then
-                dirac-dms-get-file $line
-            fi
-        fi
-    done < "tmp.txt"
-    rm "tmp.txt"
+        for energy in 91 200 360 500
+        do
+            echo "Downloading: ${jobDescription}, ${evtType}, Detector Model ${detModel}, Reco Stage ${recoStage}, ${energy}GeV"
+            cd /r06/lc/sg568/HCAL_Optimisation_Studies/RootFilesBatch/Detector_Model_${detModel}/Reco_Stage_${recoStage}/${evtType}/${energy}GeV
+            dirac-ilc-find-in-FC /ilc/user/s/sgreen JobDescription=${jobDescription} Energy=${energy} EvtType=${evtType} Type=Rec MokkaJobNumber=${detModel} ReconstructionVariant=${recoStage} > "tmp.txt"
+            while read line
+            do
+                if [[ $line == *'.root'* ]] && [[ $line == *'Default'* ]]
+                then
+                    if [ ! -f "${line##*/}" ] && [ ! -f "/r06/lc/sg568/${jobDescription}/Detector_Model_${detModel}/Reco_Stage_${recoVar}/${i}GeV/${line##*/}" ];
+                    then
+                        echo "Downloading file: $line"
+                        dirac-dms-get-file $line
+                    fi
+                fi
+            done < "tmp.txt"
+        done
+    done
 done
--- MarlinJobs/DiracDownloads/MetaDataSetting.py
+++ MarlinJobs/DiracDownloads/MetaDataSetting.py
@@ -6,14 +6,16 @@ from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 
 evtType = 'Z_uds'
 jobDescription = 'OptimisationStudies'
-detNumbers = [38, 39, 40, 41, 42, 49, 50, 51, 60, 61, 62, 63]
+detNumbers = [38, 39] #, 40, 41, 42, 49, 50, 51, 60, 61, 62, 63]
 recoStages = [69, 70, 71, 72, 73, 74, 75, 76]
 fileType = 'Rec'
 
 energies = [91,200,360,500]
 
 fc = FileCatalogClient()
-for energy in energies:
-    path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-    pathdict = {'path':path, 'meta':{'Energy':energy, 'EvtType':evtType, 'JobDescription':jobDescription, 'MokkaJobNumber':detNumber, 'ReconstructionVariant':recoStage, 'Type':fileType}}
-    res = fc.setMetadata(pathdict['path'], pathdict['meta'])
+for detNumber in detNumbers:
+    for recoStage in recoStages:
+        for energy in energies:
+            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
+            pathdict = {'path':path, 'meta':{'Energy':energy, 'EvtType':evtType, 'JobDescription':jobDescription, 'MokkaJobNumber':detNumber, 'ReconstructionVariant':recoStage, 'Type':fileType}}
+            res = fc.setMetadata(pathdict['path'], pathdict['meta'])
