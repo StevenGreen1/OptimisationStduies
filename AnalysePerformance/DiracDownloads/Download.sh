0178fff0591b45d5d34039881baa88bf1ae62b80
Steven Green <sg1sg2sg3@hotmail.co.uk>
2015-10-21 12:11:13 +0100

    Added scripts for downloading results files from the grid.

---
+++ AnalysePerformance/DiracDownloads/Download.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ../env.sh
+
+evtType=Z_uds
+jobDescription=OptimisationStudies
+
+for detModel in 38 {45..77}
+do
+    for recoStage in {69..76}
+    do
+        for energy in 91 200 360 500
+        do
+            echo "Downloading: ${jobDescription}, ${evtType}, Detector Model ${detModel}, Reco Stage ${recoStage}, ${energy}GeV"
+            cd /r06/lc/sg568/HCAL_Optimisation_Studies/AnalysePerformanceResults/Detector_Model_${detModel}/Reco_Stage_${recoStage}/${evtType}/${energy}GeV
+            dirac-ilc-find-in-FC /ilc/user/s/sgreen JobDescription=${jobDescription} Energy=${energy} EvtType=${evtType} Type=Results MokkaJobNumber=${detModel} ReconstructionVariant=${recoStage} > "tmp.txt"
+            while read line
+            do
+                if [[ ! -f "${line##*/}" ]];
+                then
+                    echo "Downloading file: $line"
+                    dirac-dms-get-file $line
+                fi
+            done < "tmp.txt"
+            rm "tmp.txt"
+        done
+    done
+done
---
+++ AnalysePerformance/DiracDownloads/MetaDataSetting.py
@@ -0,0 +1,21 @@
+import os
+
+from DIRAC.Core.Base import Script
+Script.parseCommandLine()
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+
+evtType = 'Z_uds'
+jobDescription = 'OptimisationStudies'
+detNumbers = [38, 39, 40, 41, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77]
+recoStages = [69, 70, 71, 72, 73, 74, 75, 76]
+fileType = 'Results'
+
+energies = [91,200,360,500]
+
+fc = FileCatalogClient()
+for detNumber in detNumbers:
+    for recoStage in recoStages:
+        for energy in energies:
+            path = '/ilc/user/s/sgreen/' + jobDescription + '/AnalysePerformance/Detector_Model_' + str(detNumber) + '/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
+            pathdict = {'path':path, 'meta':{'Energy':energy, 'EvtType':evtType, 'JobDescription':jobDescription, 'MokkaJobNumber':detNumber, 'ReconstructionVariant':recoStage, 'Type':fileType}}
+            res = fc.setMetadata(pathdict['path'], pathdict['meta'])
