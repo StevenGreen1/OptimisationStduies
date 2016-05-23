4ae5f1c138bd62758d2970b8b492284220ee7fa8
Steven Green <sg1sg2sg3@hotmail.co.uk>
2016-05-23 10:14:03 +0100

    Updating MetaDataSetting.py script.

--- MarlinJobs/DiracDownloads/MetaDataSetting.py
+++ MarlinJobs/DiracDownloads/MetaDataSetting.py
@@ -9,9 +9,9 @@ detNumbers = range(84,104)
 recoStages = [38, 63, 71] 
 fileType = 'Rec'
 
-eventsToSimulate = [ { 'EventType': "Z_uds", 'Energies': [91, 200, 360, 500] },
-                       'EventType': "Kaon0L", 'Energies': [10, 20, 50, 100, 200, 500] },
-                       'EventType': "Photon", 'Energies': [10, 20, 50, 100, 200, 500] }
+eventsToSimulate = [ { 'EventType': 'Z_uds', 'Energies': [91, 200, 360, 500] } ,
+                     { 'EventType': 'Kaon0L', 'Energies': [10, 20, 50, 100, 200, 500] },
+                     { 'EventType': 'Photon', 'Energies': [10, 20, 50, 100, 200, 500] }
                    ]
 
 fc = FileCatalogClient()
