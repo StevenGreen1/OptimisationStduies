0616068e31d3dd043df2aed3ccfde78085b42977
Steven Green <sg1sg2sg3@hotmail.co.uk>
2016-05-24 09:08:54 +0100

    Updated scripts.

--- MarlinJobs/DiracDownloads/MetaDataRemoving.py
+++ MarlinJobs/DiracDownloads/MetaDataRemoving.py
@@ -4,43 +4,27 @@ from DIRAC.Core.Base import Script
 Script.parseCommandLine()
 from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 
-#evtType = 'Z_uds'
-evtType = 'Photon'
-jobDescription = 'OptimisationStudies'
-detNumbers = range(84,104) 
-#recoStages = [38, 69, 70, 71, 72, 73, 74, 75, 76]  
-recoStages = [71]  
+jobDescription = 'OptimisationStudies_ECalStudies'
+#detNumbers = range(84,104)
+detNumbers = range(90,96) + range(100,104)
+#recoStages = [63]
+recoStages = [38, 63, 71]
+fileType = 'Rec'
 
-#energies = [91,200,360,500]
-energies = [100]
+eventsToSimulate = [ { 'EventType': 'Z_uds', 'Energies': [91, 200, 360, 500] } ,
+                     { 'EventType': 'Kaon0L', 'Energies': [10, 20, 50, 100, 200, 500] },
+                     { 'EventType': 'Photon', 'Energies': [10, 20, 50, 100, 200, 500] }
+                   ]
 
 fc = FileCatalogClient()
 
 for detNumber in detNumbers:
     for recoStage in recoStages:
-        for energy in energies:
-#            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-#            metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
-#            metaDict = {path:metadata}
-#            result = fc.removeMetadata(metaDict)
-#
-#            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '_Run2/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-#            metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
-#            metaDict = {path:metadata}
-#            result = fc.removeMetadata(metaDict)
-#
-#            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '_Run3/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-#            metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
-#            metaDict = {path:metadata}
-#            result = fc.removeMetadata(metaDict)
-#
-#            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '_Run4/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-#            metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
-#            metaDict = {path:metadata}
-#            result = fc.removeMetadata(metaDict)
-#
-            path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '_Run5/Reco_Stage_' + str(recoStage) + '/' + evtType + '/' + str(energy) + 'GeV'
-            metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
-            metaDict = {path:metadata}
-            result = fc.removeMetadata(metaDict)
+        for eventSelection in eventsToSimulate:
+            eventType = eventSelection['EventType']
+            for energy in eventSelection['Energies']:
+                path = '/ilc/user/s/sgreen/' + jobDescription + '/MarlinJobs/Detector_Model_' + str(detNumber) + '/Reco_Stage_' + str(recoStage) + '/' + eventType + '/' + str(energy) + 'GeV'
+                metadata = ['Energy','EvtType','JobDescription','MokkaJobNumber','ReconstructionVariant','Type']
+                metaDict = {path:metadata}
+                result = fc.removeMetadata(metaDict)
 
