426ebd5008f00f272e5abd48624f48cd7be27439
Steven Green <sg1sg2sg3@hotmail.co.uk>
2016-05-26 09:55:35 +0100

    Added energy resolution download scripts.

--- EnergyResolution/DiracDownloads/Download.sh
+++ EnergyResolution/DiracDownloads/Download.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-source ../env.sh
+source ../../env.sh
 
-evtType=Kaon0L
-jobDescription=OptimisationStudies
+jobDescription=OptimisationStudies_ECalStudies
 
-for detModel in {38..43}
+for evtType in "Kaon0L" "Photon"
 do
-    for recoStage in {69..76}
+    for detModel in {84..103}
     do
-        echo "Downloading: ${jobDescription}, ${evtType}, Detector Model ${detModel}, Reco Stage ${recoStage}"
-        cd /r06/lc/sg568/HCAL_Optimisation_Studies/EnergyResolutionResults/Detector_Model_${detModel}/Reco_Stage_${recoStage}/${evtType}
-        dirac-ilc-find-in-FC /ilc/user/s/sgreen JobDescription=${jobDescription} EvtType=${evtType} Type=EResResults MokkaJobNumber=${detModel} ReconstructionVariant=${recoStage} > "tmp.txt"
-        while read line
+        for recoStage in 71 38 63
         do
-            if [[ $line == *'.root'* ]] && [[ $line == *'Default'* ]]
-            then
-                if [ ! -f "${line##*/}" ];
+            echo "Downloading: ${jobDescription}, ${evtType}, Detector Model ${detModel}, Reco Stage ${recoStage}"
+            cd /r06/lc/sg568/HCAL_Optimisation_Studies/EnergyResolutionResults/Detector_Model_${detModel}/Reco_Stage_${recoStage}/${evtType}
+            dirac-ilc-find-in-FC /ilc/user/s/sgreen JobDescription=${jobDescription} EvtType=${evtType} Type=EResResults MokkaJobNumber=${detModel} ReconstructionVariant=${recoStage} > "tmp.txt"
+            while read line
+            do
+                if [[ $line == *'.root'* ]] && [[ $line == *'Default'* ]]
                 then
-                    echo "Downloading file: $line"
-                    dirac-dms-get-file $line
+                    if [ ! -f "${line##*/}" ];
+                    then
+                        echo "Downloading file: $line"
+                        dirac-dms-get-file $line
+                    fi
                 fi
-            fi
-        done < "tmp.txt"
+            done < "tmp.txt"
+        done
     done
 done
 
-
-#for detModel in {45..77}
-#do
-#    for recoVar in 71
-#    do
-#        python SubmitEnergyResolutionAnalysis.py ${detModel} ${recoVar}
-#    done
-#done
